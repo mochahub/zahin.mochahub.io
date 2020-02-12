fbfdd3237b92d7b8345ea4195cb94e03b919b7ba
Zahin Mohammad <zahin.dev@gmail.com>
2020-02-12 17:05:49 -0500

    cleanup app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import GitHubCalendar from 'react-github-calendar';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//       <GitHubCalendar username="zahin-mohammad" />
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
@@ -24,9 +8,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
-import { render } from "@testing-library/react";
 
 class App extends React.Component {
 
@@ -38,94 +20,49 @@ class App extends React.Component {
   }
 
   render(){
-    switch(this.state.route) {
-      case 'about':
-        return (
-          <html>
-            <header>
-              <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                crossorigin="anonymous"
-              />
-            </header>
-            <body>
-            
-            <Router>
-            {/*
-            <Navbar bg="dark" variant="dark">
+      return (
+        <html>
+          <header>
+            <link
+              rel="stylesheet"
+              href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+              integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+              crossorigin="anonymous"
+            />
+          </header>
+          <body>
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand onClick = {()=> this.setState ({route:"home"})}>
+                  <img
+                    src="/logo.svg"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="Logo"
+                  />
+                </Navbar.Brand>
               <Nav className="mr-auto">
-                <Nav.Link>
-                  <Link to="/about">About Me</Link>
-                </Nav.Link>
-                
-                <Nav.Link href="#about">About Me</Nav.Link>
-                <Nav.Link href="#projects">Projects</Nav.Link>
-                <Nav.Link href="#pricing">Career</Nav.Link>
+                <Nav.Link onClick = {()=> this.setState ({route:"about"})}>About Me</Nav.Link>
+                <Nav.Link onClick = {()=>this.setState ({route:"projects"})}>Projects</Nav.Link>
+                <Nav.Link onClick = {()=>this.setState ({route:"career"})}>Career</Nav.Link>
               </Nav>
-            </Navbar> */}
-              {/* A <Switch> looks through its children <Route>s and
-                  renders the first one that matches the current URL. */}
-              <Redirect push to="/users" />
-              <Switch>
-                <Route path="/about">
-                  <About />
-                </Route>
-                <Route path="/users">
-                  <Users />
-                </Route>
-                <Route path="/">
-                  <Home />
-                </Route>
-              </Switch>
-            </Router>
-            </body>
-          </html>
+            </Navbar>
+
+          <Router>
+            {this.state.route === "career" && <Redirect to="/career"/>}
+            {this.state.route === "about" && <Redirect to="/about"/>}
+            {this.state.route === "projects" && <Redirect to="/projects"/>}
+            {this.state.route === "home" && <Redirect to="/"/>}
+            <Switch>
+              <Route path="/about"> <About /> </Route>
+              <Route path="/career"> <Career /> </Route>
+              <Route path="/projects"> <Projects /> </Route>
+              <Route path="/"> <Home /> </Route>
+            </Switch>
+          </Router>
+          </body>
+        </html>
         )
-    }
-    // return (
-    //   <html>
-    //     <header>
-    //     <link
-    //       rel="stylesheet"
-    //       href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    //       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-    //       crossorigin="anonymous"
-    //     />
-    //     </header>
-    //     <body>
-        
-    //     <Router>
-    //     <Navbar bg="dark" variant="dark">
-    //       {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
-    //       <Nav className="mr-auto">
-    //         <Nav.Link>
-    //           <Link to="/about">About Me</Link>
-    //         </Nav.Link>
-            
-    //         <Nav.Link href="#about">About Me</Nav.Link>
-    //         <Nav.Link href="#projects">Projects</Nav.Link>
-    //         <Nav.Link href="#pricing">Career</Nav.Link>
-    //       </Nav>
-    //     </Navbar>
-    //       {/* A <Switch> looks through its children <Route>s and
-    //           renders the first one that matches the current URL. */}
-    //       <Switch>
-    //         <Route path="/about">
-    //           <About />
-    //         </Route>
-    //         <Route path="/users">
-    //           <Users />
-    //         </Route>
-    //         <Route path="/">
-    //           <Home />
-    //         </Route>
-    //       </Switch>
-    //     </Router>
-    //     </body>
-    //   </html>
-    // );
   }
 }
 
@@ -135,10 +72,14 @@ function Home() {
   return <h2>Home</h2>;
 }
 
+function Projects() {
+  return <h2>Projects</h2>;
+}
+
 function About() {
   return <h2>About</h2>;
 }
 
-function Users() {
-  return <h2>Users</h2>;
+function Career() {
+  return <h2>Career</h2>;
 }
