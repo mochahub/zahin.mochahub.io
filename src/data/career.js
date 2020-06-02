0f54b1ac1cb1630b131b4ed6d142cd7df9ceb4b7
Zahin Mohammad <zahin.mohammad@gmail.com>
2020-06-02 00:22:06 -0400

    added careers page

---
+++ src/data/career.js
@@ -0,0 +1,38 @@
+export const cards = [
+    {
+        "company":"Lumina (acquired by BitGo)",
+        "role": "Backend Software Developer",
+        "city":"San Francisco",
+        "stateProvince": "California",
+        "start": '2019-09-01',
+        "end" : '2019-12-30',
+        "desc" : `Lumina is a portfolio management software for digital assets. At Lumina I assisted with implementing new cryptocurrency exchanges / blockchain explorers into the product. An integration included being able to scrape all of a users transaction history, a users account details and lastly the instruments supported along with their historical prices.
+        \nSome of the more interesting integrations where exchanges that held derivative instruments such as perpetual futures options, staking coins, and borrowed coins as they all provided their own interesting impact on a users realized gains (for tax purposes).   
+        \nA project that I led to completion was the integration of all Compound tokens and the infrastructure to retrieve their historical price. The integration was publicized [here](https://medium.com/luminaapp/compound-now-available-on-lumina-introducing-defi-accounting-tax-6b174c3b809d).
+        \nThroughout this internship I primarily used \`golang\`, \`postgresql\`, \`graphql\`, and \`gooq\`,
+        `
+    },
+    {
+        "company":"Aterica",
+        "role": "Backend Software Developer",
+        "city":"Waterloo",
+        "stateProvince":"Ontario",
+        "start": '2019-01-01',
+        "end" : '2019-04-30',
+        "desc" : `Aterica is the creator of the \`Veta Smart Case\` and app, which is a smart epi-pen case. At Aterica, I worked on the backend development team and assisted in fixing bugs and adding additional infrastructure logging. My favorite feature that I helped develop was a geo-fence feature that would alert a user if they walked outside of range of their epi-pen.
+        \nThroughout this internship I primarily used \`Java\`, \`Spring\` and \`AWS\`.
+        `
+    },
+    {
+        "company":"University of Waterloo Psychology",
+        "role": "Android Developer",
+        "city":"Waterloo",
+        "stateProvince":"Ontario",
+        "start": '2017-09-01',
+        "end" : '2017-12-30',
+        "desc" : `At the UW psychology department, I helped create an children's game for an Android tablet that had rigged outcomes. The purpose of the game was to analyze a child's response before and after the game. The game simulated an online opponent, and would win or lose dependant on the game parameters set by the researchers before hand. The game collected analytics data such as how fast the child played, and text and audio that they wanted to convey to their opponent.
+        \nThroughout this internship I primarily used \`Java\`.
+        `
+    }
+    
+]
--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,13 +1,58 @@
 import Container from 'react-bootstrap/Container';
 import React, { Component } from 'react';
+import { Timeline, TimelineEvent } from 'react-event-timeline'
+import { cards } from '../data/career';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+import Theme from '../styles';
+import ReactMarkdown from 'react-markdown';
+
+
+const timeLineEventStyle = {
+    backgroundColor: Theme.background,
+    fontSize: "20px",
+    color: Theme.background,
+}
+const contentStyle ={
+    borderRadius:"16px"
+}
+const titleStyle={
+    color:Theme.text
+}
 
 export default class Career extends Component {
     render() {
         return (
             <Container>
-                <div>
-                    Under Construction. Please view my <a href="https://www.linkedin.com/in/zahin-m/" target="_blank" rel="noopener noreferrer">linkedIn</a> for the time being.
-                </div>
+                <Timeline style={{ color: Theme.background }}>
+
+                    {cards.map((card) => {
+                        return (
+                        <TimelineEvent 
+                        collapsible
+                        showContent
+                            title={
+                                <ReactMarkdown
+                                source={`## ${card.role} \n ### ${card.company}`}
+                                escapeHtml={false}
+                                />}
+                            subtitle={
+                                <ReactMarkdown
+                                source={`#### ${card.city}, ${card.stateProvince} \n ###### ${card.start} - ${card.end}`}
+                                escapeHtml={false}
+                                />}
+                            style={timeLineEventStyle}
+                            contentStyle={contentStyle}
+                            titleStyle={titleStyle}
+                            icon={<FontAwesomeIcon icon={faCode} color="var(--background)" />}
+                        >
+                            <ReactMarkdown
+                                source={card.desc}
+                                escapeHtml={false}
+                            />
+                        </TimelineEvent>)
+                    })}
+                </Timeline>
             </Container>
         )
     }
