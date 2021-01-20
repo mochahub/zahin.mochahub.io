7c1a701d90988f020a5b1953583bbae68962f96e
zahin <zahin.dev@gmail.com>
2021-01-20 14:42:18 -0500

    Add BitGo to Career's
    closes #10

--- src/data/career.js
+++ src/data/career.js
@@ -1,12 +1,27 @@
 export const cards = [
     {
+        "company":"BitGo",
+        "role": "Software Engineering Intern",
+        "city":"Remote",
+        "stateProvince": "Remote",
+        "start": '2020-09-01',
+        "end" : '2020-12-30',
+        "desc" : `[BitGo](https://www.bitgo.com/) offers Institutional digital asset custody, trading, and finance. 
+        At BitGo I worked in the portfolio team, assisting in migrating the aquired app Lumina into the BitGo stack and ecosystem for a unified experience both from the UI/UX and from the backend data model perspective. 
+        I worked closesly with developers across different teams and tech stacks to create the project timeline, technical design doccuments, and code to migrate users.
+        During my internship I was in charge of this project as my quarterly goal along with assisitng in the daily bug fixes/fires, and have led it to completion. 
+        The most challenging aspect of this migration was making it backwards compatible for clients that have chosen to not migrate to the new experience while minimzing the number of code paths present in the system.
+        \nThroughout this internship I primarily used \`golang\`, \`typescript\`, \`javascript\`, \`postgresql\`, \`graphql\`, and \`gooq\`,
+        `
+    },
+    {
         "company":"Lumina (acquired by BitGo)",
-        "role": "Backend Software Developer",
+        "role": "Software Engineering Intern",
         "city":"San Francisco",
         "stateProvince": "California",
         "start": '2019-09-01',
         "end" : '2019-12-30',
-        "desc" : `Lumina is a portfolio management software for digital assets. At Lumina I assisted with implementing new cryptocurrency exchanges / blockchain explorers into the product. An integration included being able to scrape all of a users transaction history, a users account details and lastly the instruments supported along with their historical prices.
+        "desc" : `[Lumina](https://www.lumina.app/) is a portfolio management software for digital assets. At Lumina I assisted with implementing new cryptocurrency exchanges / blockchain explorers into the product. An integration included being able to scrape all of a users transaction history, a users account details and lastly the instruments supported along with their historical prices.
         \nSome of the more interesting integrations where exchanges that held derivative instruments such as perpetual futures options, staking coins, and borrowed coins as they all provided their own interesting impact on a users realized gains (for tax purposes).   
         \nA project that I led to completion was the integration of all Compound tokens and the infrastructure to retrieve their historical price. The integration was publicized [here](https://medium.com/luminaapp/compound-now-available-on-lumina-introducing-defi-accounting-tax-6b174c3b809d).
         \nThroughout this internship I primarily used \`golang\`, \`postgresql\`, \`graphql\`, and \`gooq\`,
@@ -14,18 +29,20 @@ export const cards = [
     },
     {
         "company":"Aterica",
-        "role": "Backend Software Developer",
+        "role": "Backend Software Engineering Intern",
         "city":"Waterloo",
         "stateProvince":"Ontario",
         "start": '2019-01-01',
         "end" : '2019-04-30',
-        "desc" : `Aterica is the creator of the \`Veta Smart Case\` and app, which is a smart epi-pen case. At Aterica, I worked on the backend development team and assisted in fixing bugs and adding additional infrastructure logging. My favorite feature that I helped develop was a geo-fence feature that would alert a user if they walked outside of range of their epi-pen.
+        "desc" : `[Aterica](https://www.aterica.com/) is the creator of the \`Veta Smart Case\` and app, which is a smart epi-pen case. 
+        At Aterica, I worked on the backend development team and assisted in fixing bugs and adding additional infrastructure logging. 
+        My favorite feature that I helped develop was a geo-fence feature that would alert a user if they walked outside of range of their epi-pen.
         \nThroughout this internship I primarily used \`Java\`, \`Spring\` and \`AWS\`.
         `
     },
     {
         "company":"University of Waterloo Psychology",
-        "role": "Android Developer",
+        "role": "Android App Development Intern",
         "city":"Waterloo",
         "stateProvince":"Ontario",
         "start": '2017-09-01',
@@ -34,5 +51,4 @@ export const cards = [
         \nThroughout this internship I primarily used \`Java\`.
         `
     }
-    
 ]
